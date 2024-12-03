0001fe033f3dfbf0f41fdb3e2910d521c4a66633
Jeel Patel <itsjeel01@gmail.com>
2024-12-03 12:11:43 +0530

    Refactor port and connection log

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import { app } from './app.js';
 import logger from './utils/logger.js';
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
-const server = app.listen(port, () => {
-    logger.info(`App running on ${port} port`);
+app.listen(port, () => {
+    logger.info(`App running on port: ${port}`);
 });
