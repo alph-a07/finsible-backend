2de93010fae54b719350f3b66c93600cdc71df27
Jeel Patel <itsjeel01@gmail.com>
2024-12-04 14:02:47 +0530

    Refactor double quotes to single quotes

--- src/app.js
+++ src/app.js
@@ -1,27 +1,27 @@
-import express from "express";
-import morgan from "morgan";
-import mongoose from "mongoose";
-import logger from "./utils/logger.js";
-import router from "./routes/routes.js";
+import express from 'express';
+import morgan from 'morgan';
+import mongoose from 'mongoose';
+import logger from './utils/logger.js';
+import router from './routes/routes.js';
 
 export const app = express();
 
-const morganFormat = ":method :url :status :response-time ms";
-if (process.env.NODE_ENV != "production") {
+const morganFormat = ':method :url :status :response-time ms';
+if (process.env.NODE_ENV != 'production') {
   app.use(
     morgan(morganFormat, {
       stream: {
         write: (message) => {
           const logObject = {
-            method: message.split(" ")[0],
-            url: message.split(" ")[1],
-            status: message.split(" ")[2],
-            responseTime: message.split(" ")[3],
+            method: message.split(' ')[0],
+            url: message.split(' ')[1],
+            status: message.split(' ')[2],
+            responseTime: message.split(' ')[3],
           };
           logger.info(
             JSON.stringify(logObject)
-              .replaceAll(",", ", ")
-              .replaceAll(":", ": "),
+              .replaceAll(',', ', ')
+              .replaceAll(':', ': '),
           );
         },
       },
@@ -32,17 +32,17 @@ if (process.env.NODE_ENV != "production") {
 // Middleware to parse JSON bodies
 app.use(express.json());
 
-app.use("/", router);
+app.use('/', router);
 
 const uri = process.env.DB_URL;
 const clientOptions = {
-  serverApi: { version: "1", strict: true, deprecationErrors: true },
+  serverApi: { version: '1', strict: true, deprecationErrors: true },
 };
 
 try {
   await mongoose.connect(uri, clientOptions);
   await mongoose.connection.db.admin().command({ ping: 1 });
-  logger.info("Connection to MongoDB Successful.");
+  logger.info('Connection to MongoDB Successful.');
 } catch (error) {
-  logger.error("Error connecting to MongoDB", error);
+  logger.error('Error connecting to MongoDB', error);
 }
